18755fa25e4c8d9c86796448c1cb1357480018bf
David <dphennessy2@gmail.com>
2019-04-06 16:22:34 +0100

    Changed checkbutton style

--- src/Backgammon/Winscreen.java
+++ src/Backgammon/Winscreen.java
@@ -2,7 +2,6 @@ package Backgammon;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 
@@ -13,9 +12,11 @@ public class Winscreen {
     public Label winnerName;
     public Label player1;
     public Label player2;
+    public Label title;
 
     void setup(Player winner, Player loser) {
         winnerName.setText(winner.getPlayerName().toUpperCase() + " WINS!");
+        title.setText("Playing up to " + Player.upto);
         player1.setText(winner.getScore() > loser.getScore() ? winner.toString() : loser.toString());
         player2.setText(loser.getScore() < winner.getScore() ? loser.toString() : winner.toString());
         winner.reset();
@@ -24,7 +25,7 @@ public class Winscreen {
     }
 
     public void click(ActionEvent actionEvent) throws IOException {
-        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("board.fxml")), 1000, 715);
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("login.fxml")), 1000, 715);
         scene.getStylesheets().addAll(this.getClass().getResource("application.css").toExternalForm());
         Main.window.setScene(scene);
     }
